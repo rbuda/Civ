a2b1168e4b1e11fe69e17edeee3ac3fe6ef3a5ba
rbuda <ryannebuda@gmail.com>
2016-02-12 12:35:49 -0600

    Merged on Windows

--- generator.rb
+++ generator.rb
@@ -1,6 +1,6 @@
 civs = ['America','Arabia','Assyria','Austria','Aztec','Babylon','Brazil','Byzantium','Carthage','Celts','China','Denmark','Egypt','England','Ethiopia','France','Germany','Greece','Huns','Inca','India','Indonesia','Iroquois','Japan','Korea','Maya','Mongolia','Morocco','Netherlands','Ottomans','Persia','Poland','Polynesia','Portugal','Rome','Russia','Shoeshone','Siam','Songhai','Spain','Sweden','Venice','Zulu']
 
-$maps = ['Pangea','Small Continents', 'Large Continents', 'Pangea Plus', 'Continents Plus', 'Continents']
+$maps = ['pangea', 'pangea plus' ,'continents plus', 'continents', 'highlands']
 
 # Gather User Names
 def users
@@ -17,8 +17,6 @@ def users
 	end
 end
 
-
-# !!!!!!!!!!!PSEUDOCODE THIS!!!!!!!!!!!!
 # Change Maps
 def terrain(answer)
 	answer.capitalize!
@@ -30,48 +28,61 @@ def terrain(answer)
 		puts 'Either put y or n ya dope!'
 	end
 end
-
 def elim_maps
 	puts 'Which ones?'
-	answer2 = gets.chomp.to_s
-		revised_maps = $maps.delete(answer2)
+	if answer2 = gets.chomp.downcase.to_s
+		$maps.delete(answer2)		
 	else 	
-	# WHY WON'T MODIFIED MAPS PRINT??????
-	puts revised_maps
 	end
 end
 
+def dots
+	puts '...................'
+end
+
 # User Prompt:
-# puts 'How many people are playing?'
-# @number_of_players = gets.chomp.to_i
-# puts
-# 	if @number_of_players < 4
-# 		puts 'Ha! You have no friends!'
-# 	else
-# 		puts 'Wow popular today!'
-# 	end
-# puts
-# sleep 1.25 
-
-# Call users Method
-# users
-# puts 'Calculating Order From First To Last...'
-# puts
-# # sleep 1.25
-# puts @users_array.shuffle!
-# puts
-
-puts '.................'
-puts $maps
-puts '.................'
+puts 'How many people are playing?'
+@number_of_players = gets.chomp.to_i
 puts
+	if @number_of_players < 4
+		puts 'Ha! You have no friends!'
+	else
+		puts 'Wow popular today!'
+	end
+puts
+sleep 1.25 
+
+users
+puts 'Calculating Order From First To Last...'
+puts
+sleep 1.25
+puts 'Here is your order:'
+puts @users_array.shuffle!
+puts
+sleep 1.25
+
+puts 'Let\'s look at our potential maps...'
+sleep 0.5
+dots
+puts 'Maps List:'
+puts $maps
+dots
+puts
+
 puts 'Any maps you don\'t like? (y or n)'
 answer = gets.chomp
+puts
 terrain(answer)
-
-# puts maps.sample
-
-# puts maps
+puts
+dots
+sleep 0.5
+puts 'Revised Maps List:'
+puts $maps
+dots
+puts
+@random_map = $maps.sample
+puts 'You drew ' + @random_map + '!'
+puts
 
 # puts 'What civs DON\'T you want to be?'
 
